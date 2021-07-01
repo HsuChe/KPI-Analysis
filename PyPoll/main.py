74f9bb948dea0145ddb19510252f80a868a2a459
Daniel Chen <hsueh.l.chen@outlook.com>
2021-07-01 17:46:36 -0400

    last commit before class

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,11 +5,9 @@ from datetime import date
 poll_csvpath = os.path.join("Resources","election_data.csv")
 analysis_path = os.path.join("analysis",f"analysis_{date.today()}")
 
-print(poll_csvpath, analysis_path,'hello world')
-
 def parser():
     with open(poll_csvpath) as file:
-        csvreader = csv.reader(file):
+        csvreader = csv.reader(file)
         csv_list = []
         for row in csvreader:
             csv_list.append(row)
